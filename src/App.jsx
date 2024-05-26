5461b9128cdf2ea54e511006d6b3706518640832
Joshimmor <Joshimmor@gmail.com>
2024-05-26 11:08:01 -0400

    ugh

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <div className='block' id='top'>
+         <p class="headline">
+           Emerald Dwarf Rasboras (Microrasbora erythromicron) are small, vibrant freshwater fish native to the pristine waters of Inle Lake, Myanmar. Known for their striking emerald green bodies adorned with horizontal stripes and a shimmering blue hue.
+         </p> 
+      </div>
+      <div className='block' id='middle'>
+        <p className='headline'>
+          However, the natural home of the Emerald Dwarf Rasboras is under severe threat. Industrial pollution, agricultural runoff, and deforestation are degrading the water quality in the Inle Lake region. Factories and farms dump pollutants into the waterways, leading to toxic conditions that the fish cannot survive. Additionally, deforestation causes sediment to wash into the lake, disrupting the natural environment and reducing the habitat available for these fish.
+        </p>
+      </div>
+      <div className='block' id='bottom'>
+        <p className='headline'>
+          To protect the Emerald Dwarf Rasboras, it is crucial to enforce stricter pollution controls and promote sustainable agricultural practices. Conservation efforts, such as establishing protected areas and raising awareness about the environmental impacts of pollution, are essential. As aquarists, we can help by maintaining healthy aquarium environments that replicate their natural habitat and educating others about the importance of conserving these beautiful fish and their ecosystems.
+        </p>
+      </div>
+    </main>
+  )
+}
